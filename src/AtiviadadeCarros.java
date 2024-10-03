028452df50ebae654fc4515bc71700a3455defc3
Paulo <paulo70306@edu.unipar.br>
2024-10-02 22:03:00 -0300

    provinha nada facil

---
+++ src/AtiviadadeCarros.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class AtiviadadeCarros {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes  = new String[10];
+
+        int [] valores = new int [10];
+
+        System.out.println("Quantidade de Carros");
+
+        for (int i = 0; i < 5; i++) {
+            valores[1] = sc.nextInt();
+
+            System.out.println ("Nome do Carro" + i );
+
+        }
+
+        Scanner avaliar = new Scanner(System.in);
+
+        int[] numeros = new int[10];
+
+        System.out.println("Insira um numero: ");
+        numeros[0] = sc.nextInt();
+        System.out.println("Insira um numero: ");
+        numeros[1] = sc.nextInt();
+        System.out.println("Insira um numero: ");
+        numeros[2] = sc.nextInt();
+
+
+        System.out.println("valor 1: " + numeros[0]);
+        System.out.println("valor 2: " + numeros[1]);
+        System.out.println("valor 3: " + numeros[2]);
+
+        int nota = 0;
+        for (int i = 1; i <= 10; i++) {
+            nota += i;
+        }
+        System.out.println(" Avaliado " + nota);
+
+        if ( nota >= 6 && nota <=10 ) {
+            System.out.println("Carro bom ");
+        } else if (nota >6 ) {
+            System.out.println("Carro ruim ");
+        }
+        else if (nota >11){
+        System.out.println("Invalido");
+        }
+
+        double media = (numeros[0] + numeros[1] + numeros[2]) / 3.0;
+        System.out.println(" Nota do carro " + media);
+
+
+    }
+}
---
+++ src/AtiviadeVotacao.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class AtiviadeVotacao {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int votar = 0;
+        int num = 0;
+        int opcao = 0;
+        int votosTaffe = 0;
+        int votosRodrigo = 0;
+        int votosLucas = 0;
+        int votosAlessandro = 0;
+
+        System.out.println("informe quantos votos");
+        num = scanner.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            votar = 0;
+            do {
+
+
+                System.out.println("80 taffe");
+                System.out.println("70 rodrigo");
+                System.out.println("60 lucas");
+                System.out.println("50 alessandro");
+                votar = scanner.nextInt();
+
+
+            } while (votar != 80 && votar != 70 && votar != 60 && votar != 50);
+
+            if (votar == 80) {
+                votosTaffe++;
+            } else if (votar == 70) {
+                votosRodrigo++;
+            } else if (votar == 60) {
+                votosLucas++;
+            } else if (votar == 50) {
+                votosAlessandro++;
+            }
+
+
+        }
+
+
+        if (votosTaffe > votosRodrigo && votosTaffe > votosAlessandro && votosTaffe > votosLucas) {
+            System.out.println("Taffe");
+        }
+
+        if (votosRodrigo > votosTaffe && votosRodrigo > votosAlessandro && votosRodrigo > votosLucas ) {
+            System.out.println("Rodrigo");
+        }
+
+        if (votosLucas > votosRodrigo && votosLucas > votosAlessandro && votosLucas > votosTaffe) {
+            System.out.println("Lucas");
+        }
+
+        if (votosAlessandro > votosRodrigo && votosAlessandro > votosTaffe && votosAlessandro > votosLucas) {
+            System.out.println("Alessandro");
+        }
+    }
+}
+
+
